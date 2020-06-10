9287b1ff9e2641dbc2c288404434a7311f2611c3
V. Kovpak <cn007b@gmail.com>
2020-06-10 10:13:51 +0300

    Update strings.go

--- strings.go
+++ strings.go
@@ -7,5 +7,5 @@ import (
 
 // ToUpperFirst returns a copy of the string s with first letter mapped to its upper case.
 func ToUpperFirst(s string) string {
-	return strings.ToUpper(s[0:1]) + s[1:]
+	return strings.ToUpper(s[:1]) + s[1:]
 }
